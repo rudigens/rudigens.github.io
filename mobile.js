ecf22f91fbe208f6ddd08941ea6adc9f23176df0
Rudi Gens <rgens@Rudis-MBP.gci.net>
2019-04-23 20:57:20 -0800

    Initial commit for website

---
+++ mobile.js
@@ -0,0 +1,12 @@
+function navigate(theform)
+{
+if (theform.mobile_nav.options.research.selected == true) {top.location="research.html"}
+if (theform.mobile_nav.options.project.selected == true) {top.location="projects.html"}
+if (theform.mobile_nav.options.teaching.selected == true) {top.location="teaching.html"}
+if (theform.mobile_nav.options.software.selected == true) {top.location="software.html"}
+if (theform.mobile_nav.options.outreach.selected == true) {top.location="outreach.html"}
+if (theform.mobile_nav.options.summary.selected == true) {top.location="summary.html"}
+if (theform.mobile_nav.options.cv.selected == true) {top.location="curriculum_vitae.html"}
+if (theform.mobile_nav.options.publication.selected == true) {top.location="publications.html"}
+if (theform.mobile_nav.options.contact.selected == true) {top.location="contact.html"}
+}
